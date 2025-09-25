b57f7b2af48d03103e291b38d6e8256e50843469
Arturo Verbel <arturo-verbel@hotmail.com>
2025-09-25 13:25:21 -0500

    Just session

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
-const express = require('express');
-const productRouter = require('./controllers/product.controller.js')
+import express from 'express';
+import session from 'express-session'
 
 const app = express();
+app.use(session({
+    secret: 'secret',
+    resave: true, // Para mantener la session activa
+    saveUninitialized: true // Guardar cualquiar cosa, asi sea datos vacios
+}))
 
 app.use(express.json()); // Para leer JSON en requests
+
+const DB = [
+    {
+        name: 'Donald Trump',
+        username: 'president',
+        password: 'secret',
+        rol: 'admin'
+    }
+]
+
+app.get('/login', (req, res) => {
+    const { username, password } = req.query
+
+    const user =  DB.find(u => u.username === username && u.password === password)
+    if(!user) return res.status(400).send('Invalid credentials')
+
+    req.session.user = user
+
+    res.send('Login Success!!')
+})
+
+function authentication(req, res, next) {
+    if(req.session?.user) return next()
+
+    return res.status(401).send('error de authentication')
+}
+
+app.get('/private', authentication, (req, res) => {
+    res.send('Esta pagina la puede ver la persona logeada ' + JSON.stringify(req.session.user))
+})
 
 app.get('/', (req, res) => {
   res.send('Servidor Express funcionando 🚀');
 });
-
-app.use('/products', productRouter)
 
 const PORT = 3000;
 app.listen(PORT, () => {
